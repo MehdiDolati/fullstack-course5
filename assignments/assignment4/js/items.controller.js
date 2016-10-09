21bdec7058c4a1ddcb9bce7e2b5af98674609b9c
Mehdi <mehdi.dolati@gmail.com>
2016-10-10 07:31:50 +1100

    Update items.controller.js

--- assignments/assignment4/js/items.controller.js
+++ assignments/assignment4/js/items.controller.js
@@ -7,7 +7,6 @@ angular.module('MenuApp')
 ItemsController.$inject= ['items'];
 function ItemsController(items) {
     var catItems = this;
-    console.log(items);
     catItems.items = items[0].menu_items;
     catItems.name = items[1];
 }
