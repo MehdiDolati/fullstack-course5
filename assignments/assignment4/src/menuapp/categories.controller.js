dd08a601cf69bd5bf5ec8a691281650e6dc512a5
Mehdi <mehdi.dolati@gmail.com>
2016-10-10 06:55:44 +1100

    Update categories.controller.js

--- assignments/assignment4/src/menuapp/categories.controller.js
+++ assignments/assignment4/src/menuapp/categories.controller.js
@@ -4,9 +4,11 @@
 angular.module('MenuApp')
 .controller('CategoriesController', CategoriesController);
 
+
 CategoriesController.$inject = ['MenuDataService', 'categories'];
 function CategoriesController(MenuDataService, categories) {
   var ctrl = this;
   ctrl.categories = categories;
 }
+
 })();
