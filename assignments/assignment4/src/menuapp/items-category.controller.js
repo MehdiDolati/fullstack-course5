d3df39665a957a3bd3ba0ea9172f4260a6ea1a1c
Mehdi <mehdi.dolati@gmail.com>
2016-10-10 06:12:35 +1100

    Create items-category.controller.js

---
+++ assignments/assignment4/src/menuapp/items-category.controller.js
@@ -0,0 +1,13 @@
+(function () {
+'use strict';
+angular.module('MenuApp')
+.controller('ItemsCategoryController', ItemsCategoryController);
+
+
+ItemsCategoryController.Inject = ['items'];
+function ItemsCategoryController(items) {
+  var itemList = this;
+  itemList.items = items;
+    
+  }
+})();
