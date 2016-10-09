a3e320a6f2004cd641f639d8cba65792dc2907eb
Mehdi <mehdi.dolati@gmail.com>
2016-10-10 07:11:19 +1100

    Create categories.controller.js

---
+++ assignments/assignment4/js/categories.controller.js
@@ -0,0 +1,14 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.controller('CategoriesController', CategoriesController);
+
+CategoriesController.$inject = ['items'];
+function CategoriesController(items) {
+	console.log(items);
+	var catList = this;
+	catList.cats = items;
+}
+
+})();
